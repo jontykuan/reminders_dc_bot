aa41bc723e58aecc0e5fb8da8e47633d4db81dc5
jontykuan <kuan02050323@gmail.com>
2025-01-20 17:31:29 +0800

    Initial commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="DISCORD_TOKEN.env")
+load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 LLMSTUDIO_BASE_URL = "http://192.168.1.186:1234/v1"  # Default LM Studio endpoint
